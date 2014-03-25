5e4c894c657b7770778940b47b80e2976073c12c
Zack Brown <z@charybrown.com>
2014-03-24 19:35:32 -0700

    get transclusion and scope working so that two fa-surfaces can sit side-by-side with independent logic

--- app/scripts/directives/fa-surface.js
+++ app/scripts/directives/fa-surface.js
@@ -12,12 +12,14 @@ angular.module('integrationApp')
               $scope.$apply();
           }, 1000)
       },
-      compile: function(scope){
+      compile: function(tElem, tAttrs, transclude){
         console.log('compiling surface');
 
         return {
           pre: function(scope, element, attrs){
-            console.log('surf pre');
+            scope.modifier = new famous['famous/core/modifier']({
+              size: [500, 400]
+            });
             scope.surface = new famous['famous/core/surface']({
               properties: {
                 backgroundColor: '#FF0000'
@@ -26,31 +28,42 @@ angular.module('integrationApp')
           },
           post: function(scope, element, attrs){
             scope.updateContent = function(){
-              console.log('updateContent');
               //TODO:   There may be a more efficient way to do this than to 
               //        $interpolate and then string-compare.  Is there a way to
               //        anchor-link a div directly, for example?
-              var prospectiveContent = $interpolate(element.html())(scope);
+              //        directive DOM linking would probably need to be supported in
+              //        the famo.us engine, so for the time being, another approach could be:
+              //        1. take the raw template string before interpolating it
+              //        2. map all of the expressions inside {{}}'s into an array
+              //        3. evaluate all of those expressions and keep track of the values
+              //        4. compare all of these values of interest on each pass here,
+              //           -- only update the surface if one of those values changes    
+              var prospectiveContent = $interpolate(element.find('div').html())(scope);
               if(scope.currentContent !== prospectiveContent){ //this is a potentially large string-compare
-                console.log('updating famous surface');
                 scope.currentContent = prospectiveContent;
                 scope.surface.setContent(prospectiveContent);
               }
+              
             };
 
             //listener-free scope.$watch will fire any time a $digest occurs
-            scope.$watch(function(old, n){
+            scope.$watch(function(){
               scope.updateContent();
             })
 
             scope.updateContent();
 
-            scope.$parent.view._add(scope.surface);
+            scope.$emit('registerChild', {surf: scope.surface, mod: scope.modifier});
+
+            transclude(scope, function(clone) {
+              element.find('div').append(clone);
+            });
           }
         }
       },
       transclude: true,
-      template: '<div ng-transclude></div>',
+      scope: true,
+      template: '<div></div>',
       restrict: 'EA'
     };
   });
