0e2cb1805eb79cd945709351269b2215be903499
Zack Brown <z@charybrown.com>
2014-03-27 15:13:17 -0700

    add angular ui router; add other libraries; add demo controller and view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,17 @@
-angular.module('integrationApp',['famous.angular']);
+angular.module('integrationApp',['famous.angular', 'ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state("main", {
+        url: "/",
+        templateUrl: "views/main.html"
+      })
+      .state("demo", {
+        url: "/demo",
+        templateUrl: "views/demo.html"
+      });
+    });
 
 window.famousAngularBootstrap = function(){
   angular.bootstrap(document, ['integrationApp']);
