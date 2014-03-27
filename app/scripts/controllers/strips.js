45584258677b673ecc42b25925bc71bf39c07711
David Graunke <graunked@gmail.com>
2014-03-27 14:55:09 -0700

    Adjust strip position

--- app/scripts/controllers/strips.js
+++ app/scripts/controllers/strips.js
@@ -3,9 +3,11 @@
 angular.module('integrationApp')
   .controller('StripsCtrl', function ($scope) {
     console.log('strips ctrl init');
-    $scope.angle = -Math.PI / 8;
+    $scope.angle = -Math.PI / 10;
     $scope.stripContents = [
       "Strip #1",
-      "Strip #2"
+      "Strip #2",
+      "Strip #3",
+      "Strip #4",
     ]
   });
