8d07a0767d5498784d6761c18ced64f573827dac
David Graunke <graunked@gmail.com>
2014-03-27 19:00:19 -0700

    Add method to animate in strips

--- app/scripts/controllers/strips.js
+++ app/scripts/controllers/strips.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('integrationApp')
-  .controller('StripsCtrl', function ($scope) {
+  .controller('StripsCtrl', function ($scope, famous) {
+    var Transitionable = famous["famous/transitions/transitionable"];
     console.log('strips ctrl init');
+    $scope.xPosition = new Transitionable(0);
+
+    window.slideIn = function() {
+      $scope.xPosition.set(-300);
+      $scope.xPosition.set(0, {duration: 500, curve: 'easeOut'});
+
+    };
+
     $scope.angle = -Math.PI / 10;
     $scope.stripContents = [
       "Friends",
