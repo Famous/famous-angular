7e06e8d77bd08185a80aa999b5479fef1173daf3
Zack Brown <z@charybrown.com>
2014-04-01 20:05:04 -0700

    remove unnecessary scope from app.js; set up prototype of binding to an attribute using  instead of passing through with isolate scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('integrationApp')
   .controller('MainCtrl', function ($scope, famous) {
     var EventHandler = famous['famous/core/EventHandler']
-
+    $scope.prim = 1234
     $scope.enginePipe = new EventHandler();
   });
 
