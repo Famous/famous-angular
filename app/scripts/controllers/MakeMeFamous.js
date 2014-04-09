c2f65723ab5298190404cc3ee150451d98ee6d00
David Graunke <graunked@gmail.com>
2014-04-08 18:29:13 -0700

    Organize controller

--- app/scripts/controllers/MakeMeFamous.js
+++ app/scripts/controllers/MakeMeFamous.js
@@ -3,57 +3,13 @@
 angular.module('integrationApp')
   .controller('MakeMeFamousCtrl', function ($scope, $http, $firebase, famous, ScrollParticle, gridRows) {
 
-    var Transitionable = famous["famous/transitions/Transitionable"];
-    var GenericSync = famous['famous/inputs/GenericSync'];
-    var EventHandler = famous['famous/core/EventHandler'];
+
+    // app state 
 
     $scope.rows = [];
 
     var api = "http://ec2-54-185-128-191.us-west-2.compute.amazonaws.com/latest";
     var tweets = $http.get(api);
-
-    var promotedRef = new Firebase("https://resplendent-fire-5331.firebaseio.com/promoted");
-    var promoted = $firebase(promotedRef);
-
-    $scope.isPromoted = function(tweet) {
-      return promoted.$getIndex().indexOf(tweet.screen_name) > -1;
-    };
-
-    $scope.promote = function(tweet) {
-      promoted.$child(tweet.screen_name).$set({promoted: true});
-    }
-
-    var inThrees = function(array, fn) {
-      _.each(_.range(array.length / 3), function(i) {
-        fn(array[3*i], array[3*i+1], array[3*i+2]);
-      });
-    };
-
-    $scope.eventHandler = new EventHandler();
-    $scope.xTransitionable = new Transitionable(350);
-
-    $scope.hideDetail = function() {
-      $scope.detail = null;
-
-      $scope.xTransitionable.set(-400, {duration: 300, curve: 'easeIn'});
-      $scope.modalOpacity.set(0, {duration: 300}, function(){
-        $scope.modalZ = -10; 
-      });
-    };
-
-    $scope.showDetail = function(tweet) {
-      if ($scope.detail) return;
-      $scope.detail = tweet;
-      $scope.$apply();
-
-      $scope.modalOpacity.set(1, {duration: 300});
-      $scope.modalZ = 5;
-      $scope.xTransitionable.set(350);
-      $scope.xTransitionable.set(0, {duration: 300, curve: 'easeOut'});
-    };
-
-    $scope.modalOpacity = new Transitionable(0);
-    $scope.modalZ = -10;
 
     var margin = 10;
     var height = (320 - margin*4) / 3;
@@ -74,6 +30,40 @@ angular.module('integrationApp')
       });
     });
 
+    var promotedRef = new Firebase("https://resplendent-fire-5331.firebaseio.com/promoted");
+    var promoted = $firebase(promotedRef);
+
+    $scope.isPromoted = function(tweet) {
+      return promoted.$getIndex().indexOf(tweet.screen_name) > -1;
+    };
+
+    $scope.promote = function(tweet) {
+      promoted.$child(tweet.screen_name).$set({promoted: true});
+    }
+
+    $scope.hideDetail = function() {
+      $scope.detail = null;
+      $scope.$apply();
+    };
+
+    $scope.showDetail = function(tweet) {
+      if ($scope.detail) return;
+      $scope.detail = tweet;
+      $scope.$apply();
+    };
+
+    // view state
+    
+    var Transitionable = famous["famous/transitions/Transitionable"];
+    var GenericSync = famous['famous/inputs/GenericSync'];
+    var EventHandler = famous['famous/core/EventHandler'];
+
+    $scope.eventHandler = new EventHandler();
+    $scope.xTransitionable = new Transitionable(350);
+
+    $scope.modalOpacity = new Transitionable(0);
+    $scope.modalZ = -10;
+
     var scrollParticle = new ScrollParticle(function() { 
       return scrollParticle.getPosition() < 0; 
     });
@@ -85,5 +75,31 @@ angular.module('integrationApp')
     };
 
     $scope.grid = gridRows.positions($scope.offset);
+
+
+    // app state - view state synchronization
+
+    $scope.$watch("detail", function(newDetail) {
+      if (newDetail) {
+        $scope.modalOpacity.set(1, {duration: 300});
+        $scope.modalZ = 5;
+        $scope.xTransitionable.set(350);
+        $scope.xTransitionable.set(0, {duration: 300, curve: 'easeOut'});
+      }
+      else {
+        $scope.xTransitionable.set(-400, {duration: 300, curve: 'easeIn'});
+        $scope.modalOpacity.set(0, {duration: 300}, function(){
+          $scope.modalZ = -10; 
+        });
+      }
+    });
+
+    // helpers
+
+    var inThrees = function(array, fn) {
+      _.each(_.range(array.length / 3), function(i) {
+        fn(array[3*i], array[3*i+1], array[3*i+2]);
+      });
+    };
 
   });
