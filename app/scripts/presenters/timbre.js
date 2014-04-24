0f2100fd518acd566d74eac49eccb1fb124a3381
Zack Brown <z@charybrown.com>
2014-04-24 15:56:15 -0700

    missing file commit

---
+++ app/scripts/presenters/timbre.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('integrationApp')
+  .presenter('TimbrePres', function ($scope, famous) {
+    console.log('scope', $scope); //shares same scope as TimbreCtrl
+    console.log('bag', famous.bag._contents); //has access to items created in DOM
+
+
+    setInterval(function(){
+      famous.bag.first('animateTest').replay();
+    }, 2000);
+
+  });
