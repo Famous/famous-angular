326507465d0bd59990fdb7bbcd007e002b5256b0
Andre Levi <andrelevi09@gmail.com>
2014-05-21 12:50:31 -0700

    Add delay for Karma's watch and auto-test, so that the gulp build task has time to finish.

--- karma.conf.js
+++ karma.conf.js
@@ -60,6 +60,8 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+    // Give gulp time to re-build dist, in the src/ files change
+    autoWatchBatchDelay: 1000,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
