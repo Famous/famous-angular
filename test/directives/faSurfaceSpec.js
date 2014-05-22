10cd9b24d9c1cf1c701adb14603458ac1117037c
Andre Levi <andrelevi09@gmail.com>
2014-05-22 15:44:50 -0700

    Add fa-size test to faSurface spec.

--- test/directives/faSurfaceSpec.js
+++ test/directives/faSurfaceSpec.js
@@ -2,7 +2,7 @@
 
 describe('faSurface', function() {
   var element, $compile, $scope, $famous;
-  var compileFaSurface, getSurfaceProperties;
+  var compileFaSurface, getSurface;
 
   beforeEach(module('famous.angular'));
 
@@ -16,29 +16,44 @@ describe('faSurface', function() {
     };
 
     // faSurface must be an angular element so that .scope() can be called on it
-    getSurfaceProperties = function(faSurface) {
+    getSurface = function(faSurface) {
       var scope = faSurface.scope();
       var surface = scope.isolate[scope.$id].renderNode;
-      return surface.getProperties();
+      return surface;
     };
   }));
 
   ddescribe('should accept attribute', function() {
+     
+    it('fa-size', function() {
+      //var faSurface = $compile('<fa-surface fa-size="[300,300]"></fa-surface>')($scope);
+      var faSurface = compileFaSurface('fa-size="[300, 300]"');
+      var surface = getSurface(faSurface);
+      expect(surface.getSize()).toEqual([300, 300]);
+    });
+
   
     it("fa-background-color to set the surface's color", function() {
     // Have to escape the third level of quotes for string literals
-      var faSurface = compileFaSurface("fa-background-color='\"#97DED\"'");
-      var properties = getSurfaceProperties(faSurface);
-      expect(properties.backgroundColor).toBeDefined();
+      var faSurface = compileFaSurface('fa-background-color="\'#97DED\'"');
+      var surface = getSurface(faSurface);
+      var properties = surface.getProperties();
       expect(properties.backgroundColor).toEqual("#97DED");
     });
 
     it('fa-color', function() {
-    
+      var faSurface = compileFaSurface('fa-color="\'red\'"');
+      var surface = getSurface(faSurface);
+      var properties = surface.getProperties();
+      expect(properties.color).toEqual("red");
     });
 
-    it('fa-origin', function() {
-    
+    iit('fa-origin', function() {
+      var faSurface = compileFaSurface('fa-origin="\'[0.5, 1]\'"');
+      var surface = getSurface(faSurface);
+      var properties = surface.getProperties();
+      //console.log(pr);
+      //expect(properties.color).toEqual("red");
     });
 
     it('fa-translate', function() {
@@ -53,9 +68,6 @@ describe('faSurface', function() {
     
     });
 
-    it('fa-size', function() {
-    
-    });
 
     it('fa-class', function() {
     
