5d015ec388a94a312966dba0ff38e7ac7bdd7df0
Sasha Joseph <sasha@fuzzproductions.com>
2014-05-26 10:56:43 -0400

    test: creating a common include for test bootstrapping

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function(config) {
       'bower_components/requirejs/require.js',
       'http://code.famo.us/famous/0.2.0/famous.min.js',
       'dist/famous-angular.js',
+      'test/bootstrap/common.js',
       'test/**/*Spec.js',
 
       // Dependencies for famous-angular-examples
@@ -33,14 +34,14 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
