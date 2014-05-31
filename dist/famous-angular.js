f2d5ce965c25ceb46889890371a0cb2992a153c1
Zack Brown <z@charybrown.com>
2014-05-30 17:04:14 -0700

    update build

--- dist/famous-angular.js
+++ dist/famous-angular.js
@@ -99,6 +99,20 @@ require(requirements, function(/*args*/) {
 
     /**
      * @ngdoc method
+     * @name $famousProvider#getIsolate
+     * @module famous.angular
+     * @description
+     * Given an scope, retrieves the corresponding isolate.
+     * @param {Object} scope
+     * @returns {Object} The requested isolate
+     */
+
+    _modules.getIsolate = function(scope) {
+      return ('isolate' in scope) ? scope.isolate[scope.$id] : {};
+    };
+
+    /**
+     * @ngdoc method
      * @name $famousProvider#find
      * @module famous.angular
      * @description given a selector, retrieves
@@ -134,7 +148,7 @@ require(requirements, function(/*args*/) {
       var isolates = function(scopes) {
         var _s = [];
         angular.forEach(scopes, function(scope, i) {
-          _s[i] = scope.isolate[scope.$id];
+          _s[i] = _modules.getIsolate(scope);
         });
         return _s;
       }(scopes);
@@ -917,7 +931,7 @@ angular.module('famous.angular')
             var ImageSurface = $famous['famous/surfaces/ImageSurface'];
             var Transform = $famous['famous/core/Transform']
             var EventHandler = $famous['famous/core/EventHandler'];
-            
+
             //update properties
             //TODO:  is this going to be a bottleneck?
             scope.$watch(
@@ -948,14 +962,13 @@ angular.module('famous.angular')
               properties: isolate.getProperties()
             });
 
-            //TODO:  support ng-class
-            if(attrs.class)
+            if (attrs.class) {
               isolate.renderNode.setClasses(attrs['class'].split(' '));
-
+            }
           },
           post: function(scope, element, attrs){
             var isolate = $famousDecorator.ensureIsolate(scope);
-            
+
             var updateContent = function(){
               isolate.renderNode.setContent(attrs.faImageUrl)
             };
@@ -1543,6 +1556,65 @@ angular.module('famous.angular')
  */
 
 angular.module('famous.angular')
+  .config(['$provide', '$animateProvider', function($provide, $animateProvider) {
+    // Hook into the animation system to emit ng-class syncers to surfaces
+    $provide.decorator('$animate', ['$delegate', '$$asyncCallback', '$famous', function($delegate, $$asyncCallback, $famous) {
+
+      var Surface = $famous['famous/core/Surface'];
+
+      /**
+       * Check if the element selected has an isolate renderNode that accepts classes.
+       * @param {Array} element - derived element
+       * @return {boolean}
+       */
+      function isClassable(element) {
+        return $famous.getIsolate(element.scope()).renderNode instanceof Surface;
+      }
+
+      // Fork $animateProvider methods that update class lists with ng-class
+      // in the most efficient way we can. Delegate directly to irrelevant methods
+      // (enter, leave, move). These method forks only get invoked when:
+      // 1. The element has a directive like ng-class that is updating classes
+      // 2. The element is an fa-element with an in-scope isolate
+      // 3. The isolate's renderNode is some kind of Surface
+      return {
+        enter: $delegate.enter,
+        leave: $delegate.leave,
+        move: $delegate.move,
+        addClass: function(element, className, done) {
+          $delegate.addClass(element, className, done);
+
+          if (isClassable(element)) {
+            $famous.getIsolate(element.scope()).renderNode.addClass(className);
+          }
+        },
+        removeClass: function(element, className, done) {
+          $delegate.removeClass(element, className, done);
+
+          if (isClassable(element)) {
+            $famous.getIsolate(element.scope()).renderNode.removeClass(className);
+          }
+        },
+        setClass: function(element, add, remove, done) {
+          $delegate.setClass(element, add, remove, done);
+
+          if (isClassable(element)) {
+            var surface = $famous.getIsolate(element.scope()).renderNode;
+            // There isn't a good way to delegate down to Surface.setClasses
+            // because Angular has already negotiated the list of items to add
+            // and items to remove. Manually loop through both lists.
+            angular.forEach(add.split(' '), function(className) {
+              surface.addClass(className);
+            });
+
+            angular.forEach(remove.split(' '), function(className) {
+              surface.removeClass(className);
+            });
+          }
+        }
+      }
+    }]);
+  }])
   .directive('faSurface', ['$famous', '$famousDecorator', '$interpolate', '$controller', '$compile', function ($famous, $famousDecorator, $interpolate, $controller, $compile) {
     return {
       scope: true,
@@ -1584,18 +1656,15 @@ angular.module('famous.angular')
               properties: isolate.getProperties()
             });
 
-            //TODO:  support ng-class
-            if(attrs.class)
+            if (attrs.class) {
               isolate.renderNode.setClasses(attrs['class'].split(' '));
+            }
 
           },
           post: function(scope, element, attrs){
             var isolate = $famousDecorator.ensureIsolate(scope);
 
-            var updateContent = function(){
-//              var compiledEl = isolate.compiledEl = isolate.compiledEl || $compile(element.find('div.fa-surface').contents())(scope)
-//              isolate.renderNode.setContent(isolate.compiledEl.context);
-	            //TODO check if $compile is needed ?
+            var updateContent = function() {
 	            isolate.renderNode.setContent(element[0].querySelector('div.fa-surface'));
             };
 
