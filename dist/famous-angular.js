a7022fab6fb51fb89af3ce312a8bd10a3898bb72
Zack Brown <z@charybrown.com>
2014-06-03 16:26:19 -0700

    update build

--- dist/famous-angular.js
+++ dist/famous-angular.js
@@ -1437,10 +1437,10 @@ angular.module('famous.angular')
  * @description
  * A directive to insert a {@link https://famo.us/docs/0.1.1/core/RenderNode/ Famo.us RenderNode} that is
  * a wrapper for inserting a renderable component (like a Modifer or Surface) into the render tree.
- *
+ * allows you to pass a reference to an arbitrary render node from your controller.
  * @usage
  * ```html
- * <fa-render-node>
+ * <fa-render-node fa-node="arbitrary render node reference">
  *     <!-- content -->
  * </fa-render-node>
  * ```
