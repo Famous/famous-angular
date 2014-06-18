1603eb98811cab90197665797c850630fb5d1d63
Aaron Rice <adrice727@gmail.com>
2014-06-17 20:18:47 -0700

    feat: add state function to famousState

--- src/scripts/services/famousState.js
+++ src/scripts/services/famousState.js
@@ -9,7 +9,38 @@ angular.module('famous.angular')
     var $famousState;
     var root;
 
-   
+    /**
+     * @ngdoc method
+     * @name $famousProvider#state
+     * @module famous.angular
+     * @description
+     * Defines the states of the application
+     * @param {String} definition Defines the name of the state
+     * @param {Object} definition Defines the parameters of the state
+     */
+
+    this.state = state;
+    function state(name, definition) {
+      /** 
+       * If only a single state definition object is passed as a parameter, will
+       * attempt to extract the state name from the parameter object 
+       */
+      if ( angular.isObject(name) ) {
+        definition = name;
+      } else {
+        definition.name = name;
+      }
+      defineState(definition);
+      return this;
+    }
+
+
+
+
+
+
+
+
 
 
     root = {
