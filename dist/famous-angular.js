fedc1088aa898b65c78d472ab64131f44c74d1cc
Davis Kim <dchk90@gmail.com>
2014-06-24 17:26:50 -0700

    update build

--- dist/famous-angular.js
+++ dist/famous-angular.js
@@ -1,6 +1,6 @@
 /**
  * famous-angular - Bring structure to your Famo.us apps with the power of AngularJS. Famo.us/Angular integrates seamlessly with existing Angular and Famo.us apps.
- * @version v0.0.16
+ * @version v0.0.17
  * @link https://github.com/Famous/famous-angular
  * @license MPL v2.0
  */
@@ -404,6 +404,404 @@ angular.module('famous.angular')
     };
   });
 
+angular.module('famous.angular')
+  .provider('$famousState', function (){
+    
+    var states = {};
+    var queue = {};
+    var $famousState;
+    var root;
+    
+    this.state = state;
+    function state(name, definition) {
+      
+      if ( angular.isObject(name) ) {
+        definition = name;
+      } else {
+        definition.name = name;
+      }
+      defineState(definition);
+      return this;
+    }
+
+    function defineState(state) {
+      
+      var name = state.name;
+      if ( !angular.isString(name) || name.indexOf('@') >= 0)  {
+        throw new Error('State must have a valid name');
+      }
+      if (states.hasOwnProperty(name)) {
+        throw new Error('State ' + name + ' is already defined');
+      }
+
+      // Parent state may be defined within the name of the child state or as a separate property
+      var parentName = (name.indexOf('.') !== -1) ? name.substring(0, name.lastIndexOf('.'))
+          : ( angular.isString(state.parent) ) ? state.parent
+          : '';
+
+      if ( !!parentName && !states[parentName] ) { return queueState(state); }
+      
+      buildState(state);
+
+    }
+
+    function buildState (state) {
+      
+      angular.forEach(stateBuilder, function(buildFunction) {
+        buildFunction(state);
+      });
+
+      registerState(state);
+      updateQueue();
+    }
+
+    var stateBuilder = {
+
+      parent: function(state) {
+        if (angular.isDefined(state.parent) && state.parent) { return; }
+        // regex matches any valid composite state name
+        // would match "contact.list" but not "contacts"
+        var compositeName = /^(.+)\.[^.]+$/.exec(state.name);
+        state.parent = compositeName ? compositeName[1] : root;
+      },
+
+      url: function(state) {
+        
+        var url = state.url;
+
+        if ( !angular.isDefined(url) ) { return; }
+
+        $famousUrlRouter.registerUrl(state, url);
+
+        if ( !angular.isString(url) ) { throw new Error('url for state ' + state.name + ' must be a string')}
+        
+      },
+
+      // Keep track of the closest ancestor state that has a URL (i.e. is navigable)
+      // navigable: function(state) {
+      //   return state.url ? state : (state.parent ? state.parent.navigable : null);
+      // },
+
+      // path: function(state) {
+      //   return state.parent ? state.parent.path.concat(state) : []; // exclude root from path
+      // },
+
+      template: function(state) {
+
+        var template;
+        if ( !!state.templateUrl ) {
+          template = state.templateUrl;
+
+          if ( !angular.isString(template) || template.substr(-5) !== '.html' ) {
+            throw new Error('templateUrl must be a string pointing to an HTML document (e.g. templates/myTemp.html)');
+          }
+          state.template = {link: template};
+
+        } else if ( !!state.template ) {
+          template = state.template;
+          if ( typeof template !== 'string' ){
+            throw new Error('template must be a string containing valid HTML');
+          }
+          state.template = {html: template};
+        }
+
+      },
+
+      controller: function(state){
+
+        var controller = state.controller;
+
+        if ( !state.template && !!controller ) { 
+          throw new Error('A template must defined in order to create a controller');
+        } 
+
+        if ( !!controller && !angular.isString(controller) && !angular.isFunction(controller) ) {
+          throw new Error('Controller must be a function or reference an existing controller');
+        } 
+      },
+
+      transitions: function(state) {
+        //should be a function that returns a transition(I think)
+        var inTransitionFrom = state.inTransitionFrom;
+        var outTransitionTo = state.outTransitionTo;
+        
+        if ( !angular.isDefined(inTransitionFrom) && (!angular.isDefined(outTransitionTo)) ) { return;} 
+
+        if ( !!inTransitionFrom ) {
+          angular.forEach(inTransitionFrom, function (definition, property) {
+            if ( !!property && !angular.isString(definition) ) {
+              throw new Error('inTransitionFrom property ' + property + ' must be a string' );
+            } else {
+              state.inTransitionFrom[property] = definition || null;
+            }
+          });
+        }
+
+        if ( !!outTransitionTo ) {
+          angular.forEach(outTransitionTo, function (definition, property) {
+            if ( !!property && !angular.isString(definition)  ) {
+              throw new Error('outTransitionTo property ' + property + ' must be a string' );
+            } else {
+              
+              state.outTransitionTo[property] = definition || null;
+            }
+          });
+        }
+
+      },
+      
+      views: function(state) {
+        var views = {};
+
+        angular.forEach(angular.isDefined(state.views) ? state.views : { '': state }, function (view, name) {
+          if ( !name ) { name = '@'; }
+          if ( name.indexOf('@') === -1) { name += '@' + state.parent.name; }
+          validateView(view, name);
+          views[name] = view;
+        });
+
+        state.views = views;
+      }
+
+    };
+
+    function validateView (view) {
+      stateBuilder.template(view);
+      stateBuilder.controller(view);
+      stateBuilder.transitions(view);
+    }
+
+    function registerState(state) {
+      var name = state.name;
+      delete state.name;
+      states[name] = state;
+    }
+
+    function queueState(state) {
+      if ( !queue[state.name] ) {
+        queue[state.name] = state;
+      }
+    }
+
+    function updateQueue(){
+      for ( var name in queue ) {
+        defineState(queue[name]);
+      }
+    }
+
+    root = {
+      name : '',
+      parent: null,
+      navigable: false,
+      views: null,
+      template: null,
+      contoller: null
+    };
+    
+    this.$get = $get;
+    $get.$inject = ['$rootScope','$http', '$q', '$templateCache'];
+    function $get($rootScope, $http, $q, $templateCache){
+
+      $famousState = {
+        current: root.name, // Name of the current state
+        $current: root,
+        locals: {},
+        parent: '', // Name of the parent state
+        $prior: {}, // Prior state object
+        $template: '', // HTML template for the current state
+        inTransitionTo: '',
+        outTransitionFrom: ''
+      };
+      
+      $famousState.includes = function(state) {
+        return states[state]? true : false;
+      };
+
+      $famousState.go = function(state){
+
+        validateTransfer(state);
+          
+      };
+
+      function validateTransfer(state) {
+
+        if ( state === $famousState.current ) { return; }
+
+        if ( state === '^' && $famousState.$parent !== root ) {
+          state = $famousState.parent;
+          return validateState(state);
+        } else {
+          $rootScope.$broadcast('$stateNotFound');
+        }
+
+        if ( state.indexOf('^') === 0 && state.length > 1 ) {
+          state = $famousState.parent + state;
+          return validateState(state);
+        }
+
+        if ( state.indexOf('.') === 0 ) {
+          state = $famousState.current + state;
+          return validateState(state);
+        }
+
+        validateState(state);
+      }
+
+      function validateState (state) {
+
+        if ( $famousState.includes(state) ) {
+          transitionState(state);
+        } else {
+          $rootScope.$broadcast('$stateNotFound');
+        } 
+      }
+
+
+
+      function transitionState(state) {
+
+        $famousState.$prior = $famousState.$current;
+        $famousState.current = state;
+        $famousState.$current = states[state];
+        $famousState.parent = $famousState.$current.parent;
+        $famousState.$current.locals = updateLocals();
+
+
+        // fetchAll($famousState.$current); 
+        // fetch templates
+        // .then -> $broadcast
+
+        fetchLocalTemplates($famousState.$current.locals)
+        .then(function(data) {
+          angular.forEach(data, function(template) {
+            $famousState.$current.locals[template.name].$template = template.data;
+          });
+          return fetchTemplate($famousState.$current);
+        })
+        .then(function(template){
+          $famousState.$template = template.data;
+          $rootScope.$broadcast('$stateChangeSuccess');
+        });
+
+      }
+
+      // function fetchAll(state) {
+
+      //   var templates = [{state.name: state.template}];
+
+      //   angular.forEach(state.locals, (view, name) {
+      //     templates.push(view.template);
+      //   })
+
+      //   angular.forEach(templates,  )
+
+      //   if ( state.template.html ) {
+      //     templateRequests.push(angular.identity(state.template.html));
+      //   } else {
+      //     templateRequests.push(fetchTemplate(state.template.link, name));
+      //   }
+
+      //   angular.forEach(state.locals, function(view, viewName) {
+      //     templateRequests.push(fetchTemplates(view, viewName));
+      //   })
+
+      // }
+
+      function fetchLocalTemplates(locals) {
+
+        var templateRequests = [];
+
+        angular.forEach(locals, function(view, name) {
+          templateRequests.push(fetchTemplates(view,name));
+        });
+
+        return $q.all(templateRequests);
+      }
+
+      function updateLocals (){
+
+        var locals = {};
+
+        angular.forEach($famousState.$current.views, function (view, name) {
+          if ( name !== '@' ) {
+            locals[name] = view;
+          }
+        });
+
+        return locals;
+      }
+
+      function fetchTemplates(view, name) {
+
+        var deferred = $q.defer();
+        var template = {};
+
+        $http.get(view.template.link, {cache: $templateCache})
+        .success(function(data) {
+          template.name = name;
+          template.data = data;
+          deferred.resolve(template);
+        }).error(function(data) {
+          template.name = name;
+          template.data = data;
+          deferred.reject(template);
+        });
+
+        return deferred.promise;
+
+      }
+
+      function fetchTemplate(state) {
+
+        if ( state.template.html ) {
+          return state.template.html;
+        } else {
+          return $http.get(state.template.link, { cache: $templateCache });
+        }
+      }
+
+      return $famousState;
+
+    }
+
+});
+angular.module('famous.angular')
+  .provider('$famousUrlRouter', function(){
+
+    var $famousUrlRouter = {};
+    var testName = '';
+
+    this.test = test;
+    function test(name) {
+      testName = name;
+    }
+
+    this.$get = $get;
+    $get.$inject = ['$rootScope', '$location', '$log'];
+    function $get($rootScope, $location, $log) {
+
+      $famousUrlRouter.location = function(){
+        $log.info('Current location', $location.path());
+      };
+
+      $famousUrlRouter.name = function(){
+        $log.info(testName);
+      };
+
+      $famousUrlRouter.listen = function() {
+
+        $rootScope.$on('$locationChangeSuccess', function(){
+          $log.info('Location changed to ' + $location.path());
+        });
+      };
+
+      return $famousUrlRouter;
+
+  }
+    
+    
+
+});
 /**
  * @ngdoc directive
  * @name faAnimation
@@ -1035,6 +1433,7 @@ angular.module('famous.angular')
       scope: true,
       restrict: 'EA',
       compile: function(tElement, tAttrs, transclude){
+        console.log('compiled!')
         return {
           pre: function(scope, element, attrs){
             var isolate = $famousDecorator.ensureIsolate(scope);
@@ -2880,6 +3279,122 @@ angular.module('famous.angular')
     };
   }]);
 
+angular.module('famous.angular')
+  .directive('faRouter', ['$famous', '$famousDecorator', '$famousState', '$compile', '$controller', '$parse',
+   function ($famous, $famousDecorator, $famousState, $compile, $controller, $parse) {
+            
+    return {
+      scope: true,
+      transclude: 'true',
+      template: '<div class="fa-router"></div>',
+      restrict: 'EA',
+      compile: function(element, attrs, transclude ) {
+            var parent;
+            var initial = element.html();
+
+        return {
+          pre: function($scope, element, attrs) {
+            var isolate = $famousDecorator.ensureIsolate($scope);
+            var RenderController = $famous['famous/views/RenderController'];
+            var renderController = new RenderController();
+            
+            isolate.renderNode = isolate.renderNode || renderController; 
+            isolate.states = isolate.states || {}; 
+            isolate.currentState = isolate.currentState || 'root';
+            
+            function updateView (evt, data) {
+              console.log(data);
+
+              if(evt.targetScope.$id !== $scope.$id) {
+
+                var previousView = isolate.states[isolate.fromState];
+                var currentView = isolate.states[isolate.currentState] ;
+
+                console.log('state', isolate.currentState, 'currentView:', currentView);
+                currentView.renderNode = data.renderNode;
+                if(previousView && previousView.renderNode) {
+                  if(previousView.outTransitionTo) {
+                    var outTransitionTo = $parse(previousView.outTransitionTo);
+                    outTransitionTo(previousView.$scope, { $callback: function() {
+                      isolate.renderNode.hide( { duration: 0 }, function() {
+                        console.log('hide completed');
+                      });
+                    }});
+                  }
+                  else {
+                    isolate.renderNode.hide(null, function() {
+                      console.log('hide completed without transtions');
+                    });
+                  }
+                }
+
+                isolate.renderNode.show(currentView.renderNode, { duration: 0 }, function () {
+                  if(currentView.inTransitionFrom) {
+                    console.log(currentView.inTransitionFrom);
+                    var inTransitionFrom = $parse(currentView.inTransitionFrom);
+                    inTransitionFrom(currentView.$scope, { $callback: function() {
+                      console.log('show completed');
+                    }});
+                  }
+                  else {
+                    console.log('show completed without transtions');
+                  }
+                });
+
+                evt.stopPropagation();
+              }
+            }
+
+            $scope.$on('registerChild', updateView);
+          },
+          post: function($scope, element, attrs) {
+            var isolate = $famousDecorator.ensureIsolate($scope);
+            var currentScope, currentEl, previousEL, locals, flipperScope;
+            console.log('post:', isolate);
+            $scope.$on('$stateChangeSuccess', createView);
+              
+            function createView() {
+              if(isolate.currentState === $famousState.current) return;
+              
+              locals = isolate.states[$famousState.current] || $famousState.$current;
+
+              currentEl = '<fa-view>' + $famousState.$template + '</fa-view>';
+              element.html(currentEl);
+              if(currentScope) {
+                currentScope.$destroy();
+                currentScope = null;
+              }
+              console.log('local scope:', locals.$scope, currentEl);
+             
+              currentScope = $scope.$new();
+              isolate.fromState = isolate.currentState;
+              isolate.currentState = $famousState.current;
+              console.log(currentScope);
+
+              var link = $compile(element.contents());
+              locals.$scope = currentScope;
+              isolate.states[$famousState.current] = locals;
+              if(locals.controller) {
+                var controller = $controller(locals.controller, locals);
+                if($famousState.$current.controllerAs) {
+                  currentScope[$famousState.$current.controllerAs] = controller;
+                }
+                
+                angular.element(currentEl).data('$ngControllerController', controller);   
+              }
+
+              link(currentScope);
+
+              currentScope.$emit('$viewContentLoaded');
+            }
+
+            $scope.$emit('registerChild', isolate);
+          }
+        };
+      }
+    };
+  }
+]);
 /**
  * @ngdoc directive
  * @name faScrollView
