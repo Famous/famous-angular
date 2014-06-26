ba612b95f854d53fac31df20afadaacfbae070a0
Aaron Rice <adrice727@gmail.com>
2014-06-25 20:09:31 -0700

    feat: start work to resolve static child views

--- src/scripts/services/famousTemplate.js
+++ src/scripts/services/famousTemplate.js
@@ -5,9 +5,29 @@ angular.module('famous.angular')
 
     function resolveChildViews(mainTemplate, childViews) {
 
+      var deferred = $q.defer();
+      var templateRequests = [];
+      var statics = {};
 
+      angular.forEach(childViews, function(template) {
+        var childName = template.name.split('@')[0];
+        // TODO: Add a check to ensure the parent name matches the div tag (or not?)
+        // var parentName = template.name.split('@')[1];
+        // TODO: It should be possible to define distinct behavior for staticchild views.  In order to
+        //       do this the fa-router-view cannot be replaced as it is currently (transclusion = true).
+        //       Ideally, this should be broken out into a separate directive. 
+        var target = '<div fa-router-view="' + childName + '"></div>';
+        //TODO: Refactor so that the HTML only needs to be scanned one time
+        if ( mainTemplate.indexOf(target) !== -1 ) {
+          statics[childName] = target;
+          templateRequests.push(fetchTemplate(template, childName));
+        }
+      });
 
     }
 
+
+
+    };
 
 }]);
