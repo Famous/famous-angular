c129ea587c46ff4b9dfd64fb0b6936dc893eb92d
Aaron Rice <adrice727@gmail.com>
2014-06-25 18:18:11 -0700

    feat: add famousTemplate factory to fetch templates

---
+++ src/scripts/services/famousTemplate.js
@@ -0,0 +1,13 @@
+
+
+angular.module('famous.angular')
+  .factory('$famousTemplate', ['$http', '$templateCache', '$q', function($http, $templateCache, $q){
+
+    function resolveChildViews(mainTemplate, childViews) {
+
+
+
+    }
+
+
+}]);
