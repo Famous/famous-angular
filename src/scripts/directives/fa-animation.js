a19a93a8587897d888defa35027a310c1a7620bc
Robin Böhm <robinboehm@googlemail.com>
2014-06-27 09:18:49 +0200

    refactor: satisfy jslint
    most of the tasks ...

--- src/scripts/directives/fa-animation.js
+++ src/scripts/directives/fa-animation.js
@@ -250,99 +250,106 @@ angular.module('famous.angular')
     return {
       restrict: 'EA',
       scope: true,
-      compile: function(tElement, tAttrs, transclude){
+      compile: function (tElement, tAttrs, transclude) {
         var Transform = $famous['famous/core/Transform'];
         var Transitionable = $famous['famous/transitions/Transitionable'];
         var Easing = $famous['famous/transitions/Easing'];
         return {
-          pre: function(scope, element, attrs){
+          pre: function (scope, element, attrs) {
             var isolate = famousDecorator.ensureIsolate(scope);
           },
-          post: function(scope, element, attrs){
+          post: function (scope, element, attrs) {
             var isolate = famousDecorator.ensureIsolate(scope);
-            
-            setTimeout(function(){
+
+            setTimeout(function () {
               isolate.timeline = scope.$eval(attrs.timeline);
               isolate._trans = new Transitionable(0);
 
-              isolate.play = function(callback){
+              isolate.play = function (callback) {
                 var transition = {
                   duration: scope.$eval(attrs.duration),
                   curve: scope.$eval(attrs.curve) || 'linear'
                 };
-                isolate._trans.set(1, transition, function(){
-                  if(callback)
+                isolate._trans.set(1, transition, function () {
+                  if (callback){
                     callback();
-                  if(attrs.loop){
+                  }
+                  if (attrs.loop) {
                     //Famo.us silently breaks its transitionable if this runs in
                     //the same execution context.  Maybe a suppressed SO error somewhere?
-                    setTimeout(function(){isolate.replay(callback)}, 0);
+                    setTimeout(function () {
+                      isolate.replay(callback);
+                    }, 0);
                   }
                 });
                 //TODO:  handle $animate with a callback
-              }
-              isolate.reset = function(){
+              };
+              isolate.reset = function () {
                 isolate._trans.set(0);
-              }
-              isolate.replay = function(callback){
+              };
+              isolate.replay = function (callback) {
                 isolate.reset();
                 isolate.play(callback);
-              }
+              };
 
               //disengage is a function that
               //can unassign the event listener
-              var _disengage = undefined;
-              if(attrs.event){
-                if(_disengage)
+              var _disengage = null;
+              if (attrs.event) {
+                if (_disengage){
                   _disengage();
-                _disengage = scope.$on(attrs.event, function(evt, data){
+                }
+                _disengage = scope.$on(attrs.event, function (evt, data) {
                   var callback = data && data.callback ? data.callback : undefined;
-                  isolate.replay(callback)
-                })
+                  isolate.replay(callback);
+                });
               }
 
               var id = attrs.id;
 
-              if(isolate.timeline === undefined){
+              if (isolate.timeline === undefined) {
                 isolate.timeline = isolate._trans.get.bind(isolate._trans);
-                if(attrs.autoplay)
+                if (attrs.autoplay){
                   isolate.play();
+                }
               }
-              if(!isolate.timeline instanceof Function)
+              if (!isolate.timeline instanceof Function){
                 throw 'timeline must be a reference to a function or duration must be provided';
+              }
 
-	            /**
-	             * @ngdoc directive
-	             * @name animate
-	             * @module famous.angular
-	             * @restrict E
-	             * @description
-	             * This element is used to specify the animation of an element in a {@link api/directive/faAnimation faAnimation} directive
-	             *
-	             * @usage
-	             * ```html
-	             * <fa-animation timeline="functionThatReturnsATimelineValueBetween0And1">
-	             *  <animate targetModSelector="#topMod" field="rotateX" startValue="3.1415" endValue="0" curve="inQuad" timelineLowerBound="0" timelineUpperBound=".25" />
-	             * </fa-animation>
-	             * ```
-	             */
+
+              /**
+               * @ngdoc directive
+               * @name animate
+               * @module famous.angular
+               * @restrict E
+               * @description
+               * This element is used to specify the animation of an element in a {@link api/directive/faAnimation faAnimation} directive
+               *
+               * @usage
+               * ```html
+               * <fa-animation timeline="functionThatReturnsATimelineValueBetween0And1">
+               *  <animate targetModSelector="#topMod" field="rotateX" startValue="3.1415" endValue="0" curve="inQuad" timelineLowerBound="0" timelineUpperBound=".25" />
+               * </fa-animation>
+               * ```
+               */
 
               var animates = element[0].querySelectorAll('animate');
               var declarations = {};
 
-              for(var i = 0; i < animates.length; i++){
-                (function(){
+              for (var i = 0; i < animates.length; i++) {
+                (function () {
                   var animate = animates[i];
 
                   //DOM selector string that points to our mod of interest
-                  if(animate.attributes['targetmodselector']){
+                  if (animate.attributes.targetmodselector) {
                     //dig out the reference to our modifier
                     //TODO:  support passing a direct reference to a modifier
                     //       instead of performing a DOM lookup
-	                var modElements = angular.element(element[0].parentNode)[0].querySelectorAll(animate.attributes['targetmodselector'].value);
-                    
-                    
-                    angular.forEach(modElements, function(modElement){
+                    var modElements = angular.element(element[0].parentNode)[0].querySelectorAll(animate.attributes.targetmodselector.value);
+
+
+                    angular.forEach(modElements, function (modElement) {
                       var modScope = angular.element(modElement).scope();
                       var modifier = modScope.isolate[modScope.$id].modifier;
                       var getTransform = modScope.isolate[modScope.$id].getTransform;
@@ -350,39 +357,45 @@ angular.module('famous.angular')
                       //TODO:  won't need to special-case curve type 'linear'
                       //       once/if it exists in Easing.js
                       var curve =
-                        animate.attributes['curve'] &&
-                        animate.attributes['curve'].value !== 'linear' 
-                        ? Easing[animate.attributes['curve'].value]
-                        : function(j) {return j;}; //linear
+                        animate.attributes.curve &&
+                          animate.attributes.curve.value !== 'linear'
+                          ? Easing[animate.attributes.curve.value]
+                          : function (j) {
+                          return j;
+                        }; //linear
 
                       //assign the modifier functions
-                      if(animate.attributes['field']){
-                        var field = animate.attributes['field'].value;
+                      if (animate.attributes.field) {
+                        var field = animate.attributes.field.value;
 
                         var lowerBound =
-                        animate.attributes['timelinelowerbound']
-                          ? parseFloat(animate.attributes['timelinelowerbound'].value)
-                          : 0;
+                          animate.attributes.timelinelowerbound
+                            ? parseFloat(animate.attributes.timelinelowerbound.value)
+                            : 0;
 
                         var upperBound =
-                          animate.attributes['timelineupperbound']
-                          ? parseFloat(animate.attributes['timelineupperbound'].value)
-                          : 1;
+                          animate.attributes.timelineupperbound
+                            ? parseFloat(animate.attributes.timelineupperbound.value)
+                            : 1;
 
-                        if(!animate.attributes['startvalue'])
-                          throw 'you must provide a start value for the animation'
-                        var startValue = scope.$eval(animate.attributes['startvalue'].value);
+                        if (!animate.attributes.startvalue){
+                          throw 'you must provide a start value for the animation';
+                        }
 
-                        if(!animate.attributes['endvalue'])
-                          throw 'you must provide an end value for the animation'
-                        var endValue = scope.$eval(animate.attributes['endValue'].value);
+                        var startValue = scope.$eval(animate.attributes.startvalue.value);
+
+                        if (!animate.attributes.endvalue){
+                          throw 'you must provide an end value for the animation';
+                        }
+
+                        var endValue = scope.$eval(animate.attributes.endValue.value);
 
                         //Keep arrays of all declarations so that transformFunctions
                         //can handle all of the appropriate segments
 
                         var modDecs =
                           declarations[modScope.$id] =
-                          declarations[modScope.$id] || {};
+                            declarations[modScope.$id] || {};
                         var segments = modDecs[field] = modDecs[field] || [];
                         segments.push({
                           field: field,
@@ -394,7 +407,7 @@ angular.module('famous.angular')
                         });
 
                         //Keep modDecs[field] sorted
-                        segments.sort(function(a, b){
+                        segments.sort(function (a, b) {
                           return a.lowerBound - b.lowerBound;
                         });
 
@@ -402,10 +415,10 @@ angular.module('famous.angular')
                         //after sorting by lowerBounds, if any segment's lower bound
                         //is lower than the lower bound of any item before it, domains are
                         //overlapping
-                        for(var j = 1; j < segments.length; j++){
+                        for (var j = 1; j < segments.length; j++) {
                           var lower = segments[j].lowerBound;
-                          for(var k = 0; k < j; k++){
-                            if(lower < segments[k].upperBound){
+                          for (var k = 0; k < j; k++) {
+                            if (lower < segments[k].upperBound) {
                               throw "Animate segments have overlapping \
                                 domains for the same field (" + field + "). \
                                 At any point in the timeline, only one <animate> \
@@ -438,50 +451,53 @@ angular.module('famous.angular')
                         //       and then assign this function to that reference?)
                         //TODO:  if needed:  make this more efficient.  This is a hot-running
                         //       function and we should be able to optimize.
-                        var transformFunction = function(){
+                        var transformFunction = function () {
                           var x = isolate.timeline() || 0;
                           var relevantIndex = 0;
                           var relevantSegment = segments[relevantIndex];
 
-                          for(var j = 0; j < segments.length; j++){
+                          for (var j = 0; j < segments.length; j++) {
                             //this is the relevant segment if x is in the subdomain
-                            if(x >= segments[j].lowerBound && x <= segments[j].upperBound){
+                            if (x >= segments[j].lowerBound && x <= segments[j].upperBound) {
                               relevantSegment = segments[j];
                               break;
                             }
                             //this is the relevant segment if it is the last one
-                            if(j === segments.length - 1){
+                            if (j === segments.length - 1) {
                               relevantSegment = segments[j];
                               break;
                             }
                             //this is the relevant segment if x is greater than its upper
                             //bound but less than the next segment's lower bound
-                            if(x >= segments[j].upperBound && x < segments[j + 1].lowerBound){
+                            if (x >= segments[j].upperBound && x < segments[j + 1].lowerBound) {
                               relevantSegment = segments[j];
                               break;
                             }
                           }
 
-                          if(x <= relevantSegment.lowerBound)
+                          if (x <= relevantSegment.lowerBound){
                             return relevantSegment.startValue;
-                          if(x >= relevantSegment.upperBound)
-                            return relevantSegment.endValue; 
+                          }
+                          if (x >= relevantSegment.upperBound){
+                            return relevantSegment.endValue;
+                          }
+
                           //normalize our domain to [0, 1]
-                          var subDomain = (relevantSegment.upperBound - relevantSegment.lowerBound)
+                          var subDomain = (relevantSegment.upperBound - relevantSegment.lowerBound);
                           var normalizedX = (x - relevantSegment.lowerBound) / subDomain;
 
                           //Support interpolating multiple values, e.g. for a Scale array [x,y,z]
-                          if(Array.isArray(relevantSegment.startValue)){
+                          if (Array.isArray(relevantSegment.startValue)) {
                             var ret = [];
-                            for(var j = 0; j < relevantSegment.startValue.length; j++){
+                            for (var j = 0; j < relevantSegment.startValue.length; j++) {
                               ret.push(
                                 relevantSegment.startValue[j] + relevantSegment.curve(normalizedX)
-                                *
-                                (relevantSegment.endValue[j] - relevantSegment.startValue[j])
+                                  *
+                                  (relevantSegment.endValue[j] - relevantSegment.startValue[j])
                               );
                             }
                             return ret;
-                          }else{
+                          } else {
                             return relevantSegment.startValue
                               + relevantSegment.curve(normalizedX)
                               * (relevantSegment.endValue
@@ -491,47 +507,51 @@ angular.module('famous.angular')
 
                         var transformComponents = modDecs.transformComponents = modDecs.transformComponents || [];
 
-                        if(field === 'opacity'){
-                          modifier.opacityFrom(function(){
+                        if (field === 'opacity') {
+                          modifier.opacityFrom(function () {
                             return transformFunction();
                           });
-                        }else if (field === 'origin'){
-                          modifier.originFrom(function(){
+                        } else if (field === 'origin') {
+                          modifier.originFrom(function () {
                             return transformFunction();
                           });
-                        }else if (field === 'size'){
-                          modifier.sizeFrom(function(){
+                        } else if (field === 'size') {
+                          modifier.sizeFrom(function () {
                             return transformFunction();
                           });
-                        }else{ //transform field
+                        } else { //transform field
                           transformComponents.push({
                             field: field,
                             fn: transformFunction
-                          })
+                          });
 
-                          modifier.transformFrom(function(){
+                          modifier.transformFrom(function () {
                             var usedFields = {};
                             var mult = getTransform && getTransform() ? [getTransform()] : [];
-                            for(var j = 0; j < transformComponents.length; j++){
-                              ((function(){
+                            for (var j = 0; j < transformComponents.length; j++) {
+                              ((function () {
                                 var f = transformComponents[j].field;
-                                if(!usedFields[f]){
+                                if (!usedFields[f]) {
                                   var transVal = transformComponents[j].fn();
 
-                                  if(Array.isArray(transVal))
+                                  if (Array.isArray(transVal)){
                                     mult.push(Transform[f].apply(this, transVal));
-                                  else
-                                    mult.push(Transform[f](transVal));  
+                                  }
+                                  else{
+                                    mult.push(Transform[f](transVal));
+                                  }
                                   usedFields[f] = true;
                                 }
                               })());
                             }
 
                             //Transform.multiply fails on arrays of <=1 matricies
-                            if(mult.length === 1)
-                              return mult[0]
-                            else
+                            if (mult.length === 1){
+                              return mult[0];
+                            }
+                            else{
                               return Transform.multiply.apply(this, mult);
+                            }
                           });
                         }
                       }
@@ -539,9 +559,9 @@ angular.module('famous.angular')
                   }
 
                 })();
-                
+
               }
-            }, 1)//end setTimeout
+            }, 1);//end setTimeout
           }
 
         }
