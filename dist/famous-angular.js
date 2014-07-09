69037bb5fa14784381c2dd983b17495f5e94cf68
Zack Brown <z@charybrown.com>
2014-07-09 11:32:42 -0700

    chore: update build

--- dist/famous-angular.js
+++ dist/famous-angular.js
@@ -2178,19 +2178,19 @@ angular.module('famous.angular')
       restrict: 'A',
       scope: false,
       priority: 16,
-      compile: function() {
-        return { 
-          post: function(scope, element, attrs) {
+      compile: function () {
+        return {
+          post: function (scope, element, attrs) {
             var isolate = $famousDecorator.ensureIsolate(scope);
             isolate.index = scope.$eval(attrs.faIndex);
 
-            scope.$watch(function(){
-              return scope.$eval(attrs.faIndex)
-            }, function(){
-              isolate.index = scope.$eval(attrs.faIndex)
+            scope.$watch(function () {
+              return scope.$eval(attrs.faIndex);
+            }, function () {
+              isolate.index = scope.$eval(attrs.faIndex);
             });
           }
-        }
+        };
       }
     };
   }]);
