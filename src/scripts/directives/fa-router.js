0e7a92077f99c45faea80ac2ac37da763d8bba40
Rajat Khanna <rajatkhanna82@gmail.com>
2014-07-11 16:35:18 -0700

    refactor: change from pre and post to link

--- src/scripts/directives/fa-router.js
+++ src/scripts/directives/fa-router.js
@@ -1,125 +1,116 @@
 angular.module('famous.angular')
-  .directive('faRouter', ["$famous", "$famousDecorator","$famousState","$compile", "$controller", 
-   function ($famous, $famousDecorator,$famousState ,$compile, $controller) {
+.directive('faRouter', ["$famous", "$famousDecorator", "$famousState", "$compile", "$controller", "$parse",
+ function ($famous, $famousDecorator, $famousState, $compile, $controller, $parse) {
+  return {
+    scope: true,
+    transclude: 'true',
+    // template: '<div></div>',
+    restrict: 'ECA',
+    compile: function(element , attrs, transclude ) {
+          var parent;
+          var initial = element.html();
+
+      return   function($scope , element, attrs) {
+
+          var isolate = $famousDecorator.ensureIsolate($scope);
+          var currentScope , currentEl , previousEL, locals,flipperScope;
+          // console.log("post = ",isolate);
+          var RenderController = $famous['famous/views/RenderController'];
+          var renderController = new RenderController();
+          
+          isolate.renderNode = isolate.renderNode || renderController; 
+          isolate.states = isolate.states || {}; 
+          isolate.currentState = isolate.currentState || 'root';
+          
+          $scope.$on('registerChild', updateView);
+          $scope.$on('$stateChangeSuccess', createView);
             
-    return {
-      scope: true,
-      transclude: 'true',
-      template: '<div class="fa-router"></div>',
-      restrict: 'EA',
-      compile: function(element , attrs, transclude ) {
-            var parent;
-            var initial = element.html();
+          function updateView (evt, data) {
+            // console.log(data);
 
-        return {
-          pre : function($scope , element, attrs ) {
-            var isolate = $famousDecorator.ensureIsolate($scope);
-            var RenderController = $famous['famous/views/RenderController'];
-            var renderController = new RenderController();
-           
-            
-             isolate.renderNode = isolate.renderNode || renderController; 
-             isolate.states = isolate.states || {}; 
-             isolate.currentState = isolate.currentState || 'root';
-            
-            function updateView (evt, data) {
-              console.log(data);
+            if(evt.targetScope.$id !== $scope.$id){
 
-              if(evt.targetScope.$id !== $scope.$id){
+              var previousView = isolate.states[isolate.fromState];
+              var currentView = isolate.states[isolate.currentState] ;
 
-                var previousView = isolate.states[isolate.fromState];
-                var currentView = isolate.states[isolate.currentState] ;
-                console.log('state ',isolate.currentState,'currentView :',currentView);
-                currentView.renderNode =  data.renderNode;
-                if(previousView && previousView.renderNode){
-
-                  
-                  if(previousView.outTransitions ){
-                    if(previousView.outTransitionTo.outTransformFrom) {
-                      isolate.renderNode.outTransformFrom(previousView.outTransitions.outTransformFrom);
-                    }
-                    if( previousView.outTransitions.outOpacityFrom) {
-                      isolate.renderNode.outOpacityFrom(previousView.outTransitions.outOpacityFrom);
-                    }
-                    if( previousView.outTransitions.outOriginFrom) {
-                     isolate.renderNode.outOriginFrom(previousView.outTransitions.outOriginFrom);
-                    }
-                  }
-                  isolate.renderNode.hide(previousView.outTransitionFrom.transitions,function() {
-                    console.log('hide completed');
+              currentView.renderNode =  data.renderNode;
+              if(previousView && previousView.renderNode){
+                if(previousView.outTransitionTo) {
+                  var outTransitionToFn = $parse(previousView.outTransitionTo);
+                  outTransitionTo(previousView.$scope, { $callback : function() {
+                    isolate.renderNode.hide( { duration : 0 } ,function() {
+                      // console.log('hide completed');
+                    });
+                  }});
+                }
+                else {
+                  isolate.renderNode.hide( null ,function() {
+                    // console.log('hide completed without transtions');
                   });
                 }
-                   if(currentView.inTransitions ){
-                    if(currentView.inTransitions.inTransformTo) {
-                      isolate.renderNode.inTransformFrom(currentView.inTransitions.inTransformTo);
-                    }
-                    if( currentView.inTransitions.inOpacityTo) {
-                      isolate.renderNode.inOpacityFrom(currentView.inTransitions.inOpacityTo);
-                    }
-                    if( currentView.inTransitions.inOriginTo) {
-                     isolate.renderNode.inOriginFrom(currentView.inTransitions.inOriginTo);
-                    }
-                  }
-                isolate.renderNode.show(currentView.renderNode, currentView.inTransistionsFrom.transitions,function () {
-                  console.log('show completed');
-                });
-                evt.stopPropagation();
-              }
-                 
-            }
-            $scope.$on('registerChild', updateView);
-
-
-          },
-          post : function($scope , element, attrs) {
-
-            
-
-            var isolate = $famousDecorator.ensureIsolate($scope);
-            var currentScope , currentEl , previousEL, locals,flipperScope;
-            console.log("post = ",isolate);
-            $scope.$on('$stateChangeSuccess', createView);
-              
-            function createView() {
-              if( isolate.currentState === $famousState.current) return;
-              
-              locals =   isolate.states[$famousState.current] || $famousState.$current;
-
-              currentEl = "<fa-view>" + $famousState.$template.data +"</fa-view>";
-              element.html(currentEl);
-              if (currentScope) {
-                currentScope.$destroy();
-                currentScope = null;
-              }
-              console.log("local scope :",locals.$scope, $famousState.current);
-             
-              currentScope =  $scope.$new();
-              isolate.fromState = isolate.currentState;
-              isolate.currentState = $famousState.current;
-              console.log(currentScope);
-
-              var link  = $compile(element.contents());
-              locals.$scope =  currentScope;
-              isolate.states[$famousState.current] = locals;
-              if(locals.controller){
-                var controller = $controller(locals.controller, locals);
-                if($famousState.$current.controllerAs){
-                  currentScope[$famousState.$current.controllerAs] = controller;
-                }
                 
-                angular.element(currentEl).data('$ngControllerController',controller);   
               }
 
-              link(currentScope);
+              isolate.renderNode.show(currentView.renderNode, {duration:0},function () {
 
-              currentScope.$emit('$viewContentLoaded');
+                if(currentView.inTransitionFrom){
+                  // console.log(currentView.inTransitionFrom);
+                  var inTransitionFrom = $parse(currentView.inTransitionFrom);
+                  inTransitionFrom(currentView.$scope, {$callback : function() {
+                    // console.log('show completed');
+                  }});
+                }
+                else {
+                  // console.log('show completed without transtions');
 
+                }
+               
+              });
+              evt.stopPropagation();
+            }
+               
+          }
+          function createView() {
+            if( isolate.currentState === $famousState.current) return;
+            
+            locals =   isolate.states[$famousState.current] || $famousState.$current;
+            locals.$template = $famousState.$template;
+            // console.log('locals :', locals);
+            currentEl = "<fa-view>" + locals.$template +"</fa-view>";
+            element.html(currentEl);
+            if (currentScope) {
+              currentScope.$destroy();
+              currentScope = null;
+            }
+            // console.log("local scope :",locals.$scope, currentEl);
+           
+            currentScope =  $scope.$new();
+            isolate.fromState = isolate.currentState;
+            isolate.currentState = $famousState.current;
+            console.log(currentScope);
+
+            var link  = $compile(element.contents());
+            locals.$scope =  currentScope;
+            isolate.states[$famousState.current] = locals;
+            if(locals.controller){
+              var controller = $controller(locals.controller, locals);
+              if($famousState.$current.controllerAs){
+                currentScope[$famousState.$current.controllerAs] = controller;
+              }
+              
+              angular.element(currentEl).data('$ngControllerController',controller);   
             }
 
+            link(currentScope);
 
-            $scope.$emit('registerChild', isolate);
+            currentScope.$emit('$viewContentLoaded');
+
           }
+
+
+          $scope.$emit('registerChild', isolate);
         };
       }
+    
     };
-  }]);
+}]);
