516d107b5fd8e6fa2dd7d6bc952df50bf0096bbf
Davis Kim <dchk90@gmail.com>
2014-07-18 06:47:17 -0700

    test: test famousState

--- test/services/famousStateSpec.js
+++ test/services/famousStateSpec.js
@@ -94,4 +94,61 @@ describe('$famousState', function() {
 
     describe('updateQueue', function() {}); // deferred until childviews are implemented
   });
+
+  describe('$famousState', function() {
+    describe('$includes', function() {
+      it('should check whether a state is registered', function() {
+        $famousStateProvider.state({
+          name: 'test',
+          url: '/test',
+          template: '<div></div>',
+          inTransitionFrom: { a: 'a' },
+          outTransitionTo: { a: 'a' }
+        });
+        expect($famousState.includes('test')).toBe(true);
+        expect($famousState.includes('nonexistant')).toBe(false);
+      });
+    });
+
+    describe('go', function() {
+      beforeEach(function() {
+        $famousStateProvider
+        .state({
+          name: 'test1',
+          url: '/test1',
+          template: '<div></div>',
+          inTransitionFrom: { test2: 'test2' },
+          outTransitionTo: { test2: 'test2' }
+        })
+        .state({
+          name: 'test2',
+          url: '/test2',
+          template: '<div></div>',
+          inTransitionFrom: { test1: 'test1' },
+          outTransitionTo: { test1: 'test1' }
+        });
+      });
+
+      it('should go to a new state', function() {
+        $famousState.go('test1');
+        expect($famousState.current).toBe('test1');
+        $famousState.go('test2');
+        expect($famousState.current).toBe('test2');
+      });
+
+      it('should stay on the same state when passed the current state\'s name', function() {
+        $famousState.go('test1');
+        expect($famousState.current).toBe('test1');
+        $famousState.go('test1');
+        expect($famousState.current).toBe('test1');
+      });
+
+      xit('should broadcast a $stateNotFound event when passed an invalid state name', function() {
+        $famousState.go('test1');
+        expect($famousState.current).toBe('test1');
+        $famousState.go('somethingNonExistantOrInvalid');
+        // TODO: some async listener
+      });
+    });
+  });
 });
