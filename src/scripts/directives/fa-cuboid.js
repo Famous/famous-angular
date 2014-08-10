4fa0fbe0da31b085287c78167acd1c9d85317755
Zack Brown <z@charybrown.com>
2014-08-10 13:14:03 -0700

    wip: start refactoring to support arbitrary face content

--- src/scripts/directives/fa-cuboid.js
+++ src/scripts/directives/fa-cuboid.js
@@ -6,6 +6,8 @@
  * @description
  * This directive is used to create reusable cuboid (rectangular prism) shapes
  * with arbitrary content
+ * The order of the faces is [Front, Top, Right, Bottom, Left, Back]
+ * Order can be managed/enforced with `fa-index`
  */
 
 angular.module('famous.angular')
@@ -25,10 +27,11 @@ angular.module('famous.angular')
 
         return {
           pre: function(scope, element, attrs){
+            var isolate = $famousDecorator.ensureIsolate(scope);
+
 
             //TODO:  set up watch/observe and update everything accordingly
             var _dimens = scope.$eval(attrs.faDimensions);
-            //Construct the cuboid
 
             var PI_OVER_2 = Math.PI / 2;
             var PI = Math.PI;
@@ -38,11 +41,48 @@ angular.module('famous.angular')
               /*right */ {origin: [0,0], translate: [_dimens[0],0,0], rotate: [0, PI_OVER_2, 0], size: [_dimens[2], _dimens[1]]},
               /*bottom*/ {origin: [0,0], translate: [0,_dimens[1],0], rotate: [-PI_OVER_2, 0, 0], size: [_dimens[0], _dimens[2]]},
               /*left  */ {origin: [0,0], translate: [0,0,-_dimens[2]], rotate: [0, -PI_OVER_2, 0], size: [_dimens[2], _dimens[1]]},
-              /*rear  */ {origin: [0,0], translate: [_dimens[0],0,-_dimens[2]], rotate: [0, PI, 0], size: [_dimens[0], _dimens[1]]}
-            ]
+              /*back  */ {origin: [0,0], translate: [_dimens[0],0,-_dimens[2]], rotate: [0, PI, 0], size: [_dimens[0], _dimens[1]]}
+            ];
+
+            var _root = new RenderNode();
+            var _faces = [new RenderNode(),new RenderNode(),new RenderNode(),new RenderNode(),new RenderNode(),new RenderNode()];
+            angular.forEach(_faces, function(face){_root.add(face)});
+
+            var _children = [];
+
+            var _resequenceChildren = function(){
+
+              var sequence = [];
+
+              //first pass, populate specific indexes into their preferred spots
+              angular.forEach(_children, function(child, i){
+                if(child.index !== undefined){
+                  if(sequence[child.index] !== undefined) throw new Error("Multiple children have the same index (" + child.index + ")");
+                  sequence[child.index] = child;
+                }
+              });
+
+              var counter = 0;
+              //second pass, fill out the rest
+              angular.forEach(_children, function(child, i){
+                //do not include if already sequenced
+                if(sequence.indexOf(child) === -1){
+                  while(sequence[counter] !== undefined) { counter++ };
+                  sequence[counter] = child;
+                }
+              });
+
+              //now the sequence is in the correct order:  assign each rendernode to the _faces' rendernodes
+              
+            }
 
 
-            var _root = new RenderNode;
+            //TODO:  manage origin and align?  will this be necessary?
+
+            //Construct the cuboid
+
+
+
             angular.forEach(_faceSpecs, function(spec, i){
               var _mod = new Modifier({
                 origin: function(){return spec.origin},
@@ -56,7 +96,7 @@ angular.module('famous.angular')
               var _face = new RenderNode().add(_mod);
 
               //TODO:  set child content instead of surface
-              var _surf = new Surface({size: spec.size, properties: {backgroundColor: _colors[i]}});
+              var _surf = new Surface({size: spec.size, properties: {backgroundColor: _colors[i], backfaceVisibility: 'visible'}});
               _face.add(_surf);
               _root.add(_face);
             });
