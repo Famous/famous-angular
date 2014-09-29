5a2958206168c897a46a391d0077ff93215fdb6b
Zack Brown <z@charybrown.com>
2014-09-28 17:04:28 -0700

    refactor: break fa-animate-move out into its own directive

--- dist/famous-angular.js
+++ dist/famous-angular.js
@@ -955,6 +955,32 @@ angular.module('famous.angular')
       }
     };
   }]);
+
+//TODO:  DOCUMENT
+
+angular.module('famous.angular')
+  .directive('faAnimateMove', ["$parse", "$famousDecorator", function ($parse, $famousDecorator) {
+    return {
+      restrict: 'A',
+      scope: false,
+      priority: 16,
+      compile: function () {
+        return {
+          post: function (scope, element, attrs) {
+            var isolate = $famousDecorator.ensureIsolate(scope);
+            isolate.$$animateMoveHandler = $parse(attrs.faAnimateMove);
+
+            scope.$watch(function () {
+              return scope.$eval(attrs.faAnimateMove);
+            }, function () {
+              isolate.$$animateMoveHandler = $parse(attrs.faAnimateMove);
+              if(isolate.updateMethod) isolate.updateMethod();
+            });
+          }
+        };
+      }
+    };
+  }]);
 /**
  * @ngdoc directive
  * @name faAnimation
