56982d8e0be7037d1c864b6463e9501da640d2e8
João Ribeiro <jonnybgod@gmail.com>
2014-10-07 22:52:53 +0100

    fix: updating scrollview reseting scroll position
    When adding, removing and updating items inside a scrollview it was rebuilding the node from scratch causing it to break the scroll experience by reseting the position to 0.
    
    This way the ViewSequence is reused as according to best practices and the user experience becomes as expected.
    
    I also took the liberty to make it more performant when removing items.

--- src/scripts/directives/fa-scrollview.js
+++ src/scripts/directives/fa-scrollview.js
@@ -278,7 +278,9 @@ angular.module('famous.angular')
 
             $famousDecorator.addRole('renderable',isolate);
             isolate.show();
-
+            
+            isolate.renderNode.viewSeq = [];
+            isolate.renderNode.sequenceFrom(isolate.renderNode.viewSeq);
 
             var updateScrollview = function(init){
               // Synchronize the update on the next digest cycle
@@ -288,23 +290,14 @@ angular.module('famous.angular')
                 _children.sort(function(a, b){
                   return a.index - b.index;
                 });
-
-                var options = {
-                  array: function(_children) {
-                    var _ch = [];
-                    angular.forEach(_children, function(c, i) {
-                      _ch[i] = c.renderGate;
-                    });
-                    return _ch;
-                  }(_children)
-                };
-                //set the first page on the scrollview if
-                //specified
-                if(init)
-                  options.index = scope.$eval(attrs.faStartIndex);
-
-                var viewSeq = new ViewSequence(options);
-                isolate.renderNode.sequenceFrom(viewSeq);
+                
+                angular.forEach(_children, function(c, i) {
+                  if (!isolate.renderNode.viewSeq[i] || isolate.renderNode.viewSeq[i] !== c.renderGate) {
+                    isolate.renderNode.viewSeq[i] = c.renderGate;
+                  }
+                });
+                
+                isolate.renderNode.viewSeq.length = _children.length;
               });
             };
 
@@ -320,11 +313,15 @@ angular.module('famous.angular')
                   angular.forEach(_children, function(c) {
                     if (c.id !== childScopeId) {
                       _ch.push(c);
+                    } else {
+                      isolate.renderNode.viewSeq.splice(i, 1);
                     }
                   });
                   return _ch;
                 }(_children);
-                updateScrollview();
+                scope.$$postDigest(function(){
+                  isolate.renderNode.viewSeq.length = _children.length;
+                });
               },
               updateScrollview
             );
